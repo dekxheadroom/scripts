8241ff1d3241403481e2b91f72265958bcc745e7
dekx_headroom <derek.teo.sgp@gmail.com>
2025-03-20 10:41:45 +0800

    final assignment - image coverter to convert tiff files to jpeg files

---
+++ changeImage.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+#this script is to batch convert images in a folder to
+#.jpeg format, resolution 600 x 400 px
+
+from PIL import Image
+import os
+
+#define paths and configurations
+input_dir = "~/supplier-data/images/"
+output_dir = "~/supplier-data/images/"
+target_format = "JPEG"
+target_size = (600, 400)
+#final_angle = 90
+
+#get tiff image files in source directory
+#check if content is a tiff file
+try:
+  tiff_image_files = [
+    img 
+    for img in os.listdir(input_dir) 
+    if os.path.isfile(os.path.join(input_dir, img)) 
+    and os.path.splitext(img)[1].lower() == ".tiff" or os.path.splitext(img)[1].lower() == ".tif" 
+    ]
+except FileNotFoundError:
+  print(f"Error: Input Directory '{input_dir}' not found.")
+  exit()
+
+
+#process images
+for raw_file in tiff_image_files:
+  input_path = os.path.join(input_dir, raw_file)
+  #prefix processed file name with "new"
+  output_path = os.path.join(output_dir,os.path.splitext(raw_file)[0] + ".jpeg") 
+  try:
+    with Image.open(input_path) as raw_image:
+      raw_image.resize(target_size).convert('RGB').save(output_path, target_format)
+      print(f"Processed: {input_path} -> {output_path}")
+  except FileNotFoundError:
+    print(f"Error: Input file '{input_path} not found.")
+  except Exception as e:
+    print(f"Error processing '{input_path}: {e}")
+    print(f"  Error details: {e}")
+    continue
+
+  print("Image processing complete")
